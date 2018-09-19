d58c8e26684919a2fad0840d5657c9d8158f7f69
andr68rus <ashtmb@gmail.com>
2018-09-19 14:29:11 +0300

    msm8998-common: dont build Snapdragoncamera2

--- msm8998.mk
+++ msm8998.mk
@@ -128,7 +128,7 @@ PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Camera
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     SnapdragonCamera2
 
 PRODUCT_PACKAGES += \
