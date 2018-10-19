e2bb0acb1ae67a7e076fee509014bf53d55cd7db
andr68rus <ashtmb@gmail.com>
2018-10-19 18:21:20 +0300

    msm8998-common: add XiaomiParts

--- msm8998.mk
+++ msm8998.mk
@@ -374,7 +374,8 @@ PRODUCT_COPY_FILES += \
 
 # Xiaomi
 PRODUCT_PACKAGES += \
-    XiaomiDoze
+    XiaomiDoze \
+    XiaomiParts
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/msm8998-common/msm8998-common-vendor.mk)
