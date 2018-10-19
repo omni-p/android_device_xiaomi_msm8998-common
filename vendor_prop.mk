1a316956564ac9dea05ca7c43add676ca654e6ce
andr68rus <ashtmb@gmail.com>
2018-10-19 09:59:15 +0300

    props: update perf props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -129,6 +129,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.qti.core_ctl_max_cpu=4 \
     ro.vendor.qti.core_ctl_min_cpu=0 \
+    ro.vendor.qti.sys.fw.bservice_enable=true \
+    sys.tp.grip_enable=0 \
     ro.vendor.qti.sys.fw.bg_apps_limit=60
 
 # Radio
